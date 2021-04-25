6159dea8bfaf4a4007ec33b60a5743e801991d18
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-04-25 10:49:55 +0600

    Create reverseAstring.cpp

---
+++ reverseAstring.cpp
@@ -0,0 +1,15 @@
+string reverseWord(string str){
+
+    //Your code here
+    int n = str.length();
+    int i = 0;
+    int j = n -1;
+    while (i < j){
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+    return str;
+}
