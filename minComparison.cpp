0ab2f3f346854517dcd14fc158d575f074702530
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-04-25 13:06:09 +0600

    Update minComparison.cpp

--- minComparison.cpp
+++ minComparison.cpp
@@ -44,3 +44,41 @@ pair<int, int> minComparison(vector<int> & arr){
     }
     return {min, max};
 }
+  // total number of comparison 3n/2 - 2
+    pair<int, int> minComparison(vector<int> & arr, int start, int end){
+    int min, max;
+    if (start +1 == end){
+        if (arr[start] < arr[end]){
+            min = arr[start];
+            max = arr[end];
+            return {min, max};
+        }
+        else{
+            min = arr[end];
+            max = arr[start];
+            return {min, max};
+        }
+    }
+    if (start == end){
+        min = arr[start];
+        max = arr[start];
+        return {min, max};
+    }
+    int mid = start + (end - start)/2;
+    pair<int, int> firstP = minComparison(arr, start, mid);
+    pair<int, int> secondP = minComparison(arr, mid+ 1, end);
+    if (firstP.first < secondP.first){
+        min = firstP.first;
+    }
+    else {
+        min = secondP.first;
+    }
+    if (firstP.second > secondP.second){
+        max = firstP.second;
+    }
+    else {
+        max = secondP.second;
+    }
+    return {min, max};
+
+}
