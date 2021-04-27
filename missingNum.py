d48d0095d326871d415bbae7baa47cbd845d8a38
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-04-27 15:35:57 +0600

    Create missingNum.py

---
+++ missingNum.py
@@ -0,0 +1,7 @@
+#gauss rule
+#O(n) and space: O(1)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        expected_sum = len(nums)*(len(nums)+1)//2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
