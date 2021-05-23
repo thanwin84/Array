0792301a5c156ff1fe36ed06d6c790fa504bbf32
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-05-23 15:42:24 +0600

    Create chocolateDistribution.cpp

---
+++ chocolateDistribution.cpp
@@ -0,0 +1,18 @@
+//time: O(n) and space: O(1)
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m) {
+        //code
+        sort(a.begin(), a.end());
+        long long result = LONG_MAX;
+        int left = 0;
+        int right = m - 1;
+        while (right < n) {
+            if (a[right] - a[left] < result) result = a[right] - a[left];
+            left++;
+            right++;
+        }
+        return result;
+
+        }
+};
